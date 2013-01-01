454337fa152b534bb45fefdb764d03748171777a
Sidharth Shah <sidharth@fafadiatech.com>
2013-01-01 21:39:00 +0530

    rails app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BugNinja::Application.config.secret_token = 'eaaba8309d980bd4070692518f27c85aded522040d86ecb0cc4dd9e3a5b66ff6b15965a973f8de81ea47c901e571b19e6c3027a6810e7226986266e452ae2e86'
